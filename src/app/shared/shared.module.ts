b90dae318abf6694f3807e283aab8e6ae0c34682
Alfonso Fuentes <fuentesw3008@gmail.com>
2023-08-17 10:27:54 -0500

    First Commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    SidebarComponent,
+  ]
+})
+export class SharedModule { }
